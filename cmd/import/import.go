c28bd75c1ad97b745d12c5e1a609b3b496193850
ionous <github@ionous.net>
2021-02-07 15:37:13 -0800

    add profile reporting to importer ( why so slow? sqlite writes )

--- cmd/import/import.go
+++ cmd/import/import.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"runtime/pprof"
 	"strings"
 
 	"git.sr.ht/~ionous/iffy/ephemera/reader"
@@ -31,10 +32,19 @@ func main() {
 	flag.StringVar(&outFile, "out", "", "optional output filename (sqlite3)")
 	flag.BoolVar(&errutil.Panic, "panic", false, "panic on error?")
 	flag.BoolVar(&printStories, "log", false, "write imported stories to console")
+	var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 	flag.Parse()
 	if len(outFile) == 0 {
 		dir, _ := filepath.Split(inFile)
 		outFile = filepath.Join(dir, "ephemera.db")
+	}
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
 	}
 	if xs, e := distill(outFile, inFile); e != nil {
 		log.Fatalln(e)
@@ -62,7 +72,6 @@ func distill(outFile, inFile string) (ret []*story.Story, err error) {
 		// 0700 -> read/writable by user
 		// 0777 -> ModePerm ... read/writable by all
 		os.MkdirAll(path.Dir(outFile), os.ModePerm)
-		//
 		if db, e := sql.Open(tables.DefaultDriver, outFile); e != nil {
 			err = errutil.New("couldn't create output file", outFile, e)
 		} else {
@@ -71,13 +80,20 @@ func distill(outFile, inFile string) (ret []*story.Story, err error) {
 			if e := tables.CreateEphemera(db); e != nil {
 				err = errutil.New("couldn't create tables", outFile, e)
 			} else {
-				k := story.NewImporter(db, ds.Report)
-				if xs, e := importPath(k, inFile); e != nil {
+				if storyFiles, e := readPath(inFile); e != nil {
 					err = errutil.New("couldn't import  file", inFile, e)
 				} else {
-					ret = xs
+					k := story.NewImporter(db, ds.Report)
+					for _, storyFile := range storyFiles {
+						if v, e := storyFile.importStory(k); e != nil {
+							err = errutil.Append(err, e)
+						} else {
+							ret = append(ret, v)
+						}
+					}
+
+					reader.PrintDilemmas(log.Writer(), ds)
 				}
-				reader.PrintDilemmas(log.Writer(), ds)
 			}
 		}
 	}
@@ -85,20 +101,20 @@ func distill(outFile, inFile string) (ret []*story.Story, err error) {
 }
 
 // read a comma-separated list of files and directories
-func importPath(k *story.Importer, filePaths string) (ret []*story.Story, err error) {
+func readPath(filePaths string) (ret []storyFile, err error) {
 	split := strings.Split(filePaths, ",")
 	for _, filePath := range split {
 		if info, e := os.Stat(filePath); e != nil {
 			err = e
 		} else {
 			if !info.IsDir() {
-				if one, e := importOne(k, filePath); e != nil {
+				if one, e := readOne(filePath); e != nil {
 					err = e
 				} else {
 					ret = append(ret, one)
 				}
 			} else {
-				if many, e := importMany(k, filePath); e != nil {
+				if many, e := readMany(filePath); e != nil {
 					err = e
 				} else {
 					ret = append(ret, many...)
@@ -109,7 +125,7 @@ func importPath(k *story.Importer, filePaths string) (ret []*story.Story, err er
 	return
 }
 
-func importMany(k *story.Importer, path string) (ret []*story.Story, err error) {
+func readMany(path string) (ret []storyFile, err error) {
 	if !strings.HasSuffix(path, "/") {
 		path += "/" // for opening symbolic directories
 	}
@@ -117,7 +133,7 @@ func importMany(k *story.Importer, path string) (ret []*story.Story, err error) 
 		if e != nil {
 			err = e
 		} else if !info.IsDir() && filepath.Ext(path) == ".if" {
-			if one, e := importOne(k, path); e != nil {
+			if one, e := readOne(path); e != nil {
 				err = errutil.New("error reading", path, e)
 			} else {
 				ret = append(ret, one)
@@ -128,7 +144,8 @@ func importMany(k *story.Importer, path string) (ret []*story.Story, err error) 
 	return
 }
 
-func importOne(k *story.Importer, filePath string) (ret *story.Story, err error) {
+func readOne(filePath string) (ret storyFile, err error) {
+	log.Println("reading", filePath)
 	if f, e := os.Open(filePath); e != nil {
 		err = e
 	} else {
@@ -137,9 +154,18 @@ func importOne(k *story.Importer, filePath string) (ret *story.Story, err error)
 		if e := json.NewDecoder(f).Decode(&one); e != nil && e != io.EOF {
 			err = e
 		} else {
-			log.Println("importing", filePath)
-			ret, err = k.ImportStory(filePath, one)
+			ret = storyFile{filePath, one}
 		}
 	}
 	return
+}
+
+type storyFile struct {
+	path string
+	data reader.Map
+}
+
+func (fp *storyFile) importStory(k *story.Importer) (ret *story.Story, err error) {
+	log.Println("importing", fp.path)
+	return k.ImportStory(fp.path, fp.data)
 }
