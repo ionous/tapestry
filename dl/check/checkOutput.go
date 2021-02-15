212259d0d39061b19aad923f24320ab33858553c
ionous <github@ionous.net>
2021-02-15 15:29:28 -0800

    better print mismatched output containing newlines

--- dl/check/checkOutput.go
+++ dl/check/checkOutput.go
@@ -3,6 +3,7 @@ package check
 import (
 	"bytes"
 	"log"
+	"strings"
 
 	"git.sr.ht/~ionous/iffy/dl/core"
 	"git.sr.ht/~ionous/iffy/rt"
@@ -24,7 +25,11 @@ func (t *CheckOutput) RunTest(run rt.Runtime) (err error) {
 		if e := safe.Run(run, t.Test); e != nil {
 			err = errutil.Fmt("ng! %s test encountered error: %s", t.Name, e)
 		} else if res := buf.String(); res != t.Expect {
-			err = errutil.Fmt("ng! %s got:  %q, want: %q", t.Name, res, t.Expect)
+			if eol := '\n'; strings.ContainsRune(res, eol) || strings.ContainsRune(t.Expect, eol) {
+				err = errutil.Fmt("ng! %s have:>>>\n%s<<<\nwant:>>>\n%s<<<", t.Name, res, t.Expect)
+			} else {
+				err = errutil.Fmt("ng! %s have: %q want: %q", t.Name, res, t.Expect)
+			}
 		} else {
 			log.Printf("ok. test %s got %q", t.Name, res)
 		}
