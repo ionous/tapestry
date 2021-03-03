c2e0e123b537e7c15eadd028d9dc9517c6b518f9
ionous <github@ionous.net>
2021-03-02 21:04:52 -0800

    add a helper ( for "trying" ) which can add a single variable to the scope

---
+++ rt/scope/scopeSingleValue.go
@@ -0,0 +1,30 @@
+package scope
+
+import g "git.sr.ht/~ionous/iffy/rt/generic"
+
+type SingleValue struct {
+	name string
+	val  g.Value
+}
+
+func NewSingleValue(name string, val g.Value) *SingleValue {
+	return &SingleValue{name, val}
+}
+
+func (k *SingleValue) FieldByName(field string) (ret g.Value, err error) {
+	if found := len(field) > 0 && field == k.name; !found {
+		err = g.UnknownVariable(field)
+	} else {
+		ret = k.val
+	}
+	return
+}
+
+func (k *SingleValue) SetFieldByName(field string, val g.Value) (err error) {
+	if found := len(field) > 0 && field == k.name; !found {
+		err = g.UnknownVariable(field)
+	} else {
+		k.val = val
+	}
+	return
+}
