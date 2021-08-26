7de1a3d52ce174452597f309036df691f1f0699e
ionous <github@ionous.net>
2021-08-26 15:32:49 -0700

    wip: allow 0 length commands (ex. Always) to serialize just a single string

--- export/jsonexp/jsonexpCompactFlow.go
+++ export/jsonexp/jsonexpCompactFlow.go
@@ -12,14 +12,20 @@ func (cf *CompactFlow) AddMsg(label string, field json.RawMessage) {
 	cf.Fields = append(cf.Fields, field)
 }
 
-func (cf *CompactFlow) MarshalJSON() ([]byte, error) {
-	var i interface{}
-	if len(cf.Fields) == 1 {
-		i = cf.Fields[0]
+func (cf *CompactFlow) MarshalJSON() (ret []byte, err error) {
+	sig := cf.Sig.String()
+	if cnt := len(cf.Fields); cnt == 0 {
+		ret, err = json.Marshal(sig)
 	} else {
-		i = cf.Fields
+		var i interface{}
+		if cnt == 1 {
+			i = cf.Fields[0]
+		} else {
+			i = cf.Fields
+		}
+		ret, err = json.Marshal(map[string]interface{}{
+			sig: i,
+		})
 	}
-	return json.Marshal(map[string]interface{}{
-		cf.Sig.String(): i,
-	})
+	return
 }
