dec54cae5a912bc2663a4b0295e3a6bda71b8e1f
ionous <github@ionous.net>
2021-12-28 17:28:47 -0800

    allow blank value to be upgraded to blank values of type [ caught during the custom fancy if check ]

--- rt/safe/saferValue.go
+++ rt/safe/saferValue.go
@@ -7,18 +7,21 @@ import (
 	"git.sr.ht/~ionous/iffy/rt/meta"
 )
 
-// fix: handle differences b/t kinds, aspects, etc.?
+// if the target field (ex. a pattern local) requires text of a certain type
+// and the incoming value is untyped: convert it.
 func AutoConvert(run rt.Runtime, ft g.Field, val g.Value) (ret g.Value, err error) {
-	// if the target field (ex. a pattern local) requires text of a certain type
-	// and the incoming value is untyped: convert it.
-	if needsConversion := ft.Affinity == affine.Text && len(ft.Type) > 0 &&
-		(val.Affinity() == affine.Text && len(val.Type()) == 0) ||
-		(val.Affinity() == affine.Object); !needsConversion {
-		ret = val
-	} else {
-		// set indexed field validates the ft.Type and the val.Type match
-		// we just have to give it the proper value in the first place.
-		ret, err = run.GetField(meta.ObjectId, val.String())
+	ret = val // provisionally.
+	if ft.Affinity == affine.Text && len(ft.Type) > 0 {
+		if (val.Affinity() == affine.Text && len(val.Type()) == 0) ||
+			(val.Affinity() == affine.Object) {
+			if str := val.String(); len(str) == 0 {
+				ret = g.StringFrom("", ft.Type) // allow setting of blank values
+			} else {
+				// set indexed field validates the ft.Type and the val.Type match
+				// we just have to give it the proper value in the first place.
+				ret, err = run.GetField(meta.ObjectId, str)
+			}
+		}
 	}
 	return
 }
