7cf1ce1ce77acc81c648a77850a6785d4fdfa75a
ionous <github@ionous.net>
2022-01-03 22:53:02 -0800

    story encoder -- allow the removal of empty paragraphs ( fixes an encoding bug, and the excess arent really needed anyway ) tweak to allow external programs (ex.compact) to override the story encoder for tests

--- dl/story/storyEncoder.go
+++ dl/story/storyEncoder.go
@@ -13,15 +13,25 @@ import (
 // ie. instead of: `{"Story:":[{"Paragraph:":["Example"]},{"Paragraph:":["Example","Example"]}]}`
 // it writes: `[["Example"],["Example","Example"]]`
 // an outer array of paragraphs, where each direct child is a array of story statements.
-func Encode(src *Story) (ret interface{}, err error) {
+func Encode(src *Story) (interface{}, error) {
+	return CustomEncode(src, CompactEncoder)
+}
+
+// helper for some tests
+func CustomEncode(src *Story, encoder func(jsn.Marshaler, jsn.FlowBlock) error) (ret interface{}, err error) {
 	x := Paragraph_Slice(src.Paragraph)
-	if a, e := cout.Encode(&x, CompactEncoder); e != nil {
+	if a, e := cout.Encode(&x, encoder); e != nil {
 		err = e
 	} else {
 		var out []interface{}
 		for _, x := range a.([]interface{}) {
-			for _, v := range x.(map[string]interface{}) {
-				out = append(out, v)
+			switch els := x.(type) {
+			case string:
+				// empty paragraph. i think its fine to elide it.
+			case map[string]interface{}:
+				for _, v := range els {
+					out = append(out, v)
+				}
 			}
 		}
 		ret = out
