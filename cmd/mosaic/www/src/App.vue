b0fafa024331a8b573f64dfb0ed834911d6d0704
ionous <github@ionous.net>
2022-02-04 21:44:50 -0800

    make a resizable blockly element based on the docs

--- cmd/mosaic/www/src/App.vue
+++ cmd/mosaic/www/src/App.vue
@@ -1,12 +1,12 @@
-<template
-><img alt="Vue logo" src="./assets/logo.png" 
-  /><router-view/><Catalog :catalog="catalog" 
-  /><Blockly></Blockly
+ <template
+ ><img alt="Vue logo" src="./assets/logo.png" 
+   /><router-view/><Catalog :catalog="catalog" 
+   /><Blockly></Blockly
 ></template>
-
-<script>
-import MockCatalog from './catalog/mockCatalog.js'
-import Catalog from './catalog/Catalog.vue'
+ 
+ <script>
+ import MockCatalog from './catalog/mockCatalog.js'
+ import Catalog from './catalog/Catalog.vue'
 import Blockly from './blockly/Blockly.vue'
 import { RouterView } from 'vue-router'
 
@@ -20,3 +20,52 @@ export default {
 }
 </script>
 
+
+
+<!--   
+the blockly music maker is interesting.
+it has classes on "permanent" divs -- example the edit div --
+which default to not displaying, and then adds a global "mode" attribute to the body
+to select which ones display. nice and simple. similar the ".blockly-editor" class is 
+left: -400px; by default, and during mode-blockly it "moves" into place.
+
+they load and save the workspace at every transition.
+using: 
+  let xml = Blockly.Xml.workspaceToDom(Blockly.getMainWorkspace());
+  Blockly.Xml.domToWorkspace(button.blocklyXml, workspace);
+
+tbd: that might now work because im using "extraState"
+but could just serialize and save to memory somewhere, 
+[ ex. if file persisted ]
+
+- 
+
+.mode-edit, .mode-maker, .mode-blockly {
+  display: none;
+}
+
+[mode="maker"] .mode-maker,
+[mode="edit"] .mode-edit,
+[mode="blockly"] .mode-blockly {
+  display: block;
+}
+[mode="blockly"] .blockly-editor {
+  left: 0;
+}
+
+
+/Users/ionous/Dev/blockly/google-blockly/demos/codelab/app-complete/index.html
+https://blocklycodelabs.dev/codelabs/getting-started/index.html
+
+function enableEditMode() {
+    document.body.setAttribute('mode', 'edit');
+    document.querySelectorAll('.button').forEach(btn => {
+      btn.removeEventListener('click', handlePlay);
+      btn.addEventListener('click', enableBlocklyMode);
+    });
+  }
+   provide() {
+    
+    }
+  },
+ -->
--- cmd/mosaic/www/src/blockly/Blockly.vue
+++ cmd/mosaic/www/src/blockly/Blockly.vue
@@ -1,7 +1,82 @@
 <template
-	><div class="mk-blockly"></div
+	><div id="blockly-area" class="mk-blockly"></div
+	><div id="blockly-div" style="position: absolute"></div
 ></template>
 
 <script>
+
+import Blockly from 'blockly';
+Blockly.WorkspaceAudio.prototype.preload= function(){};
+		
+const toolbox = {
+  "kind": "flyoutToolbox",
+  "contents": [
+    {
+      "kind": "block",
+      "type": "controls_if"
+    },
+    {
+      "kind": "block",
+      "type": "controls_repeat_ext"
+    },
+    {
+      "kind": "block",
+      "type": "logic_compare"
+    },
+    {
+      "kind": "block",
+      "type": "math_number"
+    },
+    {
+      "kind": "block",
+      "type": "math_arithmetic"
+    },
+    {
+      "kind": "block",
+      "type": "text"
+    },
+    {
+      "kind": "block",
+      "type": "text_print"
+    }
+  ]
+}
+
+// https://developers.google.com/blockly/guides/configure/web/resizable
+let workspace, blocklyArea, blocklyDiv;
+
+export default {
+  mounted() {
+		workspace= Blockly.inject('blockly-div', {toolbox: toolbox});
+		blocklyArea = document.getElementById('blockly-area');
+  	blocklyDiv = document.getElementById('blockly-div');
+  	window.addEventListener('resize',this.onResize);
+  	this.onResize();
+  	Blockly.svgResize(workspace);
+  },
+	destroyed: function() {
+	  window.removeEventListener('resize', this.onResize, false);
+	},
+	methods: {
+		onResize() {
+			// Compute the absolute coordinates and dimensions of blocklyArea.
+	    var element = blocklyArea;
+	    var x = 0;
+	    var y = 0;
+	    do {
+	      x += element.offsetLeft;
+	      y += element.offsetTop;
+	      element = element.offsetParent;
+	    } while (element);
+	    // Position blocklyDiv over blocklyArea.
+	    blocklyDiv.style.left = x + 'px';
+	    blocklyDiv.style.top = y + 'px';
+	    blocklyDiv.style.width = blocklyArea.offsetWidth - 3 + 'px';
+	    blocklyDiv.style.height = blocklyArea.offsetHeight - 3 + 'px';
+	    Blockly.svgResize(workspace);
+		}
+	}
+}
+
 </script>
 
