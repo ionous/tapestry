364f89c58a108c500b1719504963f7df29f33647
ionous <github@ionous.net>
2022-05-27 20:28:35 -0700

    fix windows10 doesnt open browser ( due to a windows update bug! )

--- web/support/openBrowser.go
+++ web/support/openBrowser.go
@@ -10,16 +10,17 @@ import (
 // From Miki Tebeka: Open calls the OS default program for uri
 // http://go-wise.blogspot.com/2012/04/open-fileurls-with-default-program.html
 func OpenBrowser(uri string) {
-	var commands = map[string]string{
-		"windows": "start",
-		"darwin":  "open",
-		"linux":   "xdg-open",
+	var commands = map[string][]string{
+		"windows": {"rundll32", "url.dll,FileProtocolHandler"}, // note: using "start" broke as of windows10 1809
+		"darwin":  {"open"},
+		"linux":   {"xdg-open"},
 	}
+
 	time.Sleep(300 * time.Millisecond)
 	if run, ok := commands[runtime.GOOS]; !ok {
 		log.Printf("don't know how to open the browser on %s platform", runtime.GOOS)
 	} else {
-		cmd := exec.Command(run, uri)
+		cmd := exec.Command(run[0], append(run[1:], uri)...)
 		if e := cmd.Start(); e != nil {
 			log.Printf("error opening browser %s", e.Error())
 		}
