492034d90ae64cd54a7c12769c28d97b5c25ed84
ionous <github@ionous.net>
2022-05-27 20:31:54 -0700

    a minor test of vita build ( no concrete conclusions but this is probably closer )

--- www/vite.config.js
+++ www/vite.config.js
@@ -1,13 +1,23 @@
+const { resolve } = require('path')
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // exposes the specified object as a global to the app
   define: {
     appcfg: JSON.stringify({
       mosaic: 'http://localhost:8080',
       live: 'http://localhost:8088',
     })
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        live: resolve(__dirname, 'live/index.html'),
+        mosaic: resolve(__dirname, 'mosaic/index.html')
+      }
+    }
   }
 })
