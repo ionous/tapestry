8b5f93249133c7e614449fc725d256a3b4ade8eb
ionous <github@ionous.net>
2022-09-02 17:18:07 -0700

    wip: runtime expectation support

---
+++ dl/expect/doc.go
@@ -0,0 +1,2 @@
+// Package expect provides commands authors can use to test their stories.
+package expect
---
+++ dl/expect/expect.go
@@ -0,0 +1,17 @@
+package expect
+
+import (
+	"git.sr.ht/~ionous/tapestry/rt"
+	"github.com/ionous/errutil"
+)
+
+func (op *ExpectLines) Execute(run rt.Runtime) (err error) {
+	return errutil.New("not implemented")
+}
+func (op *ExpectString) Execute(run rt.Runtime) (err error) {
+	return errutil.New("not implemented")
+}
+
+func (op *ExpectText) Execute(run rt.Runtime) (err error) {
+	return errutil.New("not implemented")
+}
