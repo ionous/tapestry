6d94f2b1be8e0b61a705f95c4d1e750a024c201c
ionous <github@ionous.net>
2023-02-22 22:54:42 -0800

    wip: variable refactor. test some specific outputs

--- dl/core/coreEncoder_test.go
+++ dl/core/coreEncoder_test.go
@@ -5,6 +5,7 @@ import (
 
 	"git.sr.ht/~ionous/tapestry/dl/assign"
 	"git.sr.ht/~ionous/tapestry/dl/core"
+	"git.sr.ht/~ionous/tapestry/dl/list"
 	"git.sr.ht/~ionous/tapestry/jsn/cout"
 )
 
@@ -20,6 +21,65 @@ func TestCompactEncoder(t *testing.T) {
 	if have, e := cout.Marshal(out, core.CompactEncoder); e != nil {
 		t.Fatal(e)
 	} else if have != `{"Factorial num:":{"FromNumber:":1}}` {
+		t.Fatal(have)
+	}
+}
+
+func TestGetTrait(t *testing.T) {
+	out := &core.GetValue{
+		Source: core.Address{
+			Choice: core.Address_Object_Opt,
+			Value: &core.ObjectRef{
+				Name:  core.GetVariable("noun"),
+				Field: core.GetVariable("trait"),
+			},
+		}}
+
+	if have, e := cout.Marshal(out, core.CompactEncoder); e != nil {
+		t.Fatal(e)
+	} else if have != `{"Get object:":{"Object:field:":["@noun","@trait"]}}` {
+		t.Fatal(have)
+	}
+}
+
+func TestGetVariablePath(t *testing.T) {
+	out := &core.GetValue{
+		Source: core.Address{
+			Choice: core.Address_Variable_Opt,
+			Value: &core.VariableRef{
+				Name: core.T("pawn"),
+				Dot: []core.Dot{
+					&core.AtField{Field: core.T("trait")},
+				},
+			},
+		}}
+
+	// fix: this should have path syntax ( re: expressions )
+	// ex. @pawn.trait
+	if have, e := cout.Marshal(out, core.CompactEncoder); e != nil {
+		t.Fatal(e)
+	} else if have != `{"Get variable:":{"Variable:dot:":["pawn",[{"AtField:":"trait"}]]}}` {
+		t.Fatal(have)
+	}
+}
+
+// true, not a core test *shrug*
+func TestListPush(t *testing.T) {
+	out := &list.ListPush{
+		Value:  &assign.FromText{Value: core.GetVariable("obj")},
+		AtEdge: core.B(true),
+		Target: core.Address{
+			Choice: core.Address_Variable_Opt,
+			Value: &core.VariableRef{
+				Name: core.T("bounds"),
+			},
+		}}
+
+	// fix: this should have path syntax ( re: expressions )
+	// ex. @pawn.trait
+	if have, e := cout.Marshal(out, core.CompactEncoder); e != nil {
+		t.Fatal(e)
+	} else if have != `{"Push:into variable:atFront:":[{"FromText:":"@obj"},"@bounds",true]}` {
 		t.Fatal(have)
 	}
 }
