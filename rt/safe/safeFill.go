289876b40881f05f459b06497b7d5e6eb457e3a1
ionous <github@ionous.net>
2023-09-04 10:17:03 -0700

    comments

--- rt/safe/safeFill.go
+++ rt/safe/safeFill.go
@@ -6,6 +6,10 @@ import (
 	"github.com/ionous/errutil"
 )
 
+// FillRecord fill the passed record with the arguments named by keys and values
+// keys are optional, but if they exist must match the order of fields in the record;
+// there doesnt have to be be a key for every field; they can be sparse.
+// returns the passed record if there's no error
 func FillRecord(run rt.Runtime, rec *g.Record, keys []string, vals []g.Value) (ret *g.Record, err error) {
 	if nk, nv := len(keys), len(vals); nv < nk {
 		err = errutil.New("too many keys")
@@ -36,7 +40,7 @@ func FillRecord(run rt.Runtime, rec *g.Record, keys []string, vals []g.Value) (r
 					err = errutil.Fmt("%w while setting arg %d(%s)", e, i, key)
 					break
 				}
-			}
+			} // end for
 			if err == nil {
 				ret = rec
 			}
