b20f5cc47452c8bfcbac042bc971f06eebfee949
ionous <github@ionous.net>
2023-09-12 11:13:13 -0700

    change test function to match what it's being used for right now

--- dl/debug/debugExpect.go
+++ dl/debug/debugExpect.go
@@ -16,14 +16,14 @@ type GreatExpectations interface {
 }
 
 func (op *ExpectOutput) Execute(run rt.Runtime) (err error) {
-	return compareLast(run, op.Output.GetLines())
+	return compareOutput(run, op.Output.GetLines())
 }
 
 func (op *ExpectText) Execute(run rt.Runtime) (err error) {
 	if v, e := safe.GetText(run, op.Text); e != nil {
 		err = e
 	} else {
-		err = compareLast(run, []string{v.String()})
+		err = compareOutput(run, []string{v.String()})
 	}
 	return
 }
@@ -38,19 +38,18 @@ func (op *Expect) Execute(run rt.Runtime) (err error) {
 }
 
 // currently doing a matching of trailing lines rather than all lines output have to match.
-func compareLast(run rt.Runtime, match []string) (err error) {
+func compareOutput(run rt.Runtime, match []string) (err error) {
 	if x, ok := run.(GreatExpectations); ok {
-		err = compareAtLast(match, x.GetAccumulatedOutput())
+		err = compareLines(match, x.GetAccumulatedOutput())
 	}
 	return
 }
 
-func compareAtLast(match, input []string) (err error) {
-	if want, have := len(match), len(input); want > have {
+func compareLines(match, input []string) (err error) {
+	if want, have := len(match), len(input); want != have {
 		err = errutil.New("expected", want, "lines, have", have,
 			"wanted:", strings.Join(match, "; "), "have:", strings.Join(input, "; "))
 	} else {
-		input = input[have-want:]
 		for i, w := range match {
 			if h := input[i]; w != h {
 				e := errutil.Fmt("line %v mismatched. wanted '%v' have '%v'", i, w, h)
--- dl/debug/debugExpect_test.go
+++ dl/debug/debugExpect_test.go
@@ -2,20 +2,20 @@ package debug
 
 import "testing"
 
-func TestAtLast(t *testing.T) {
+func TestCompareLines(t *testing.T) {
 	match := []string{"a", "b", "c"}
 	tests := []struct {
 		okay         bool
 		match, input []string
 	}{
 		{true, match, []string{"a", "b", "c"}},
-		{true, match, []string{"x", "a", "b", "c"}},
+		{false, match, []string{"x", "a", "b", "c"}},
 		{false, match, []string{"b", "c"}},
 		{false, match, []string{""}},
 		{true, []string{""}, []string{""}},
 	}
 	for i, test := range tests {
-		e := compareAtLast(test.match, test.input)
+		e := compareLines(test.match, test.input)
 		if test.okay != (e == nil) {
 			t.Fatal(i, test, e)
 		}
