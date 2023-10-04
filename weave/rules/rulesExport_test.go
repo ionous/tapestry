865fedeb9a8fa0fbba03eed22c45fab0523bf77e
ionous <github@ionous.net>
2023-10-04 15:41:24 -0700

    fix go test ( no idea how it passed before; probably go cached the result and didnt actually try to compile the file or something )

--- weave/rules/rulesExport_test.go
+++ weave/rules/rulesExport_test.go
@@ -1,5 +1,7 @@
 package rules
 
+import g "git.sr.ht/~ionous/tapestry/rt/generic"
+
 // exports for testing
 type UpdateTracker = updateTracker
 
@@ -7,6 +9,6 @@ type UpdateTracker = updateTracker
 var Ranks = ranks
 
 // exports for testing
-func (n RuleName) RuleForPattern(ks Kinds) (ret RuleInfo, err error) {
+func (n RuleName) RuleForPattern(ks g.Kinds) (ret RuleInfo, err error) {
 	return n.ruleForPattern(ks)
 }
