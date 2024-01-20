1fbe845697d5460539ad2461dccdda2d25045332
ionous <github@ionous.net>
2024-01-19 18:34:36 -0800

    helper package for rt types

---
+++ dl/rti/rti.go
@@ -0,0 +1,15 @@
+package rti
+
+import (
+	"git.sr.ht/~ionous/tapestry/rt"
+)
+
+type Assignment = rt.Assignment
+type Execute = rt.Execute
+type BoolEval = rt.BoolEval
+type NumberEval = rt.NumberEval
+type TextEval = rt.TextEval
+type RecordEval = rt.RecordEval
+type NumListEval = rt.NumListEval
+type TextListEval = rt.TextListEval
+type RecordListEval = rt.RecordListEval
