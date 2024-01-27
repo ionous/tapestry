ef612d996da3d0db7df2be6385400ccf31c7ca8d
ionous <github@ionous.net>
2024-01-27 14:51:30 -0800

    fix license comments

--- cmd/tap/internal/base/errors.go
+++ cmd/tap/internal/base/errors.go
@@ -1,5 +1,5 @@
 // Copyright (C) 2022 - Simon Travis. All rights reserved.
-// Use of this source code is governed by the Hippocratic 2.1
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 package base
 
