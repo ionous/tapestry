1515919b894ae8795fac0c24c8bebf9ebfa2da8d
ionous <github@ionous.net>
2024-04-04 11:35:54 -0700

    tap: add "normalize" to cmd xform

--- cmd/tap/internal/cmdxform/cmdXform.go
+++ cmd/tap/internal/cmdxform/cmdXform.go
@@ -4,43 +4,92 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"strings"
 
 	"git.sr.ht/~ionous/tapestry/cmd/tap/internal/base"
 	"git.sr.ht/~ionous/tapestry/support/inflect"
 	"github.com/reiver/go-porterstemmer"
 )
 
-func run(ctx context.Context, cmd *base.Command, args []string) (_ error) {
-	for _, word := range args {
-		if cfg.stem {
-			fmt.Println("stem:", porterstemmer.StemString(word))
+func run(ctx context.Context, cmd *base.Command, args []string) (err error) {
+	if len(args) == 0 {
+		err = fmt.Errorf("%w expected at least one word to transform", base.UsageError)
+	} else if !hasAnyFlags() {
+		err = fmt.Errorf("expected at least one transform option", base.UsageError)
+	} else {
+		var fns = []func(word string) string{
+			porterstemmer.StemString,
+			inflect.Pluralize,
+			inflect.Singularize,
+			inflect.Normalize,
 		}
-		if cfg.plural {
-			fmt.Println("plural:", inflect.Pluralize(word))
-		}
-		if cfg.single {
-			fmt.Println("singular:", inflect.Singularize(word))
+		//
+		var x [1]struct{} // static assert that the lengths are the same
+		var _ = x[numOptions-len(fns)]
+
+		for _, word := range args {
+			for i, ok := range optionFlags {
+				if ok {
+					opt := options(i).String()
+					str := fns[i](word)
+					fmt.Println(opt+":", str)
+				}
+			}
 		}
 	}
-
 	return
 }
 
 var CmdXform = &base.Command{
 	Run:       run,
-	UsageLine: "tap xform [-stem] [-plural] [-single] word",
+	UsageLine: buildUsage(),
 	Flag:      buildFlags(),
 	Short:     "Transform English words",
 	Long:      `Transform English words.`,
 }
 
-var cfg = struct {
-	stem, plural, single bool
-}{}
+//go:generate stringer -type=options
+type options int
+
+const (
+	stem options = iota
+	plural
+	singular
+	normal
+	numOptions = iota
+)
+
+// set if
+var optionFlags [numOptions]bool
+
+// text written to the user for "tap help xform"
+func buildUsage() (ret string) {
+	var str strings.Builder
+	str.WriteString("tap xform")
+	for i := 0; i < numOptions; i++ {
+		opt := options(i).String()
+		str.WriteString(" [-")
+		str.WriteString(opt)
+		str.WriteRune(']')
+	}
+	str.WriteString(` "word"`)
+	return str.String()
+}
 
 func buildFlags() (fs flag.FlagSet) {
-	fs.BoolVar(&cfg.stem, "stem", false, "Print suffix of a word")
-	fs.BoolVar(&cfg.plural, "plural", false, "Print plural of a word")
-	fs.BoolVar(&cfg.single, "single", false, "Print singular of a word")
+	for i := 0; i < numOptions; i++ {
+		opt := options(i).String()
+		fs.BoolVar(&optionFlags[i], opt, false, fmt.Sprintf("Print %s of a word", opt))
+	}
+	return
+}
+
+func hasAnyFlags() (okay bool) {
+	for _, ok := range optionFlags {
+		if ok {
+			okay = true
+			break
+		}
+	}
 	return
 }
