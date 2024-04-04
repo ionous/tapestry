6c0894f6e2205c6cff06d76616a3b281cac085ba
ionous <github@ionous.net>
2024-04-03 17:48:25 -0700

    fix: relation scheduling
    mapping phase no longer exists

--- dl/story/storyRel.go
+++ dl/story/storyRel.go
@@ -14,71 +14,75 @@ func (op *DefineRelation) Execute(macro rt.Runtime) error {
 }
 
 func (op *DefineRelation) Weave(cat *weave.Catalog) error {
-	return cat.Schedule(weaver.MappingPhase, func(w weaver.Weaves, run rt.Runtime) (err error) {
+	return cat.Schedule(weaver.LanguagePhase, func(w weaver.Weaves, run rt.Runtime) (err error) {
 		if rel, e := safe.GetText(run, op.Relation); e != nil {
 			err = e
 		} else {
 			rel := rel.String()
 			switch op.Cardinality {
 			case C_RelationCardinality_OneToOne:
-				err = op.addOneToOne(w, run, rel)
+				err = op.addOneToOne(cat, run, rel)
 			case C_RelationCardinality_OneToMany:
-				err = op.addOneToMany(w, run, rel)
+				err = op.addOneToMany(cat, run, rel)
 			case C_RelationCardinality_ManyToOne:
-				err = op.addManyToOne(w, run, rel)
+				err = op.addManyToOne(cat, run, rel)
 			case C_RelationCardinality_ManyToMany:
-				err = op.addManyToMany(w, run, rel)
+				err = op.addManyToMany(cat, run, rel)
 			}
 		}
 		return
 	})
 }
 
-func (op *DefineRelation) addOneToOne(w weaver.Weaves, run rt.Runtime, rel string) (err error) {
+func (op *DefineRelation) addOneToOne(cat *weave.Catalog, run rt.Runtime, rel string) (err error) {
 	if a, e := safe.GetText(run, op.Kind); e != nil {
 		err = e
 	} else if b, e := safe.GetText(run, op.OtherKind); e != nil {
 		err = e
 	} else {
-		err = addRelation(w, rel, a.String(), b.String(), false, false)
+		err = addRelation(cat, rel, a.String(), b.String(), false, false)
 	}
 	return
 }
 
-func (op *DefineRelation) addOneToMany(w weaver.Weaves, run rt.Runtime, rel string) (err error) {
+func (op *DefineRelation) addOneToMany(cat *weave.Catalog, run rt.Runtime, rel string) (err error) {
 	if a, e := safe.GetText(run, op.Kind); e != nil {
 		err = e
 	} else if b, e := safe.GetText(run, op.OtherKind); e != nil {
 		err = e
 	} else {
-		err = addRelation(w, rel, a.String(), b.String(), false, true)
+		err = addRelation(cat, rel, a.String(), b.String(), false, true)
 	}
 	return
 }
 
-func (op *DefineRelation) addManyToOne(w weaver.Weaves, run rt.Runtime, rel string) (err error) {
+func (op *DefineRelation) addManyToOne(cat *weave.Catalog, run rt.Runtime, rel string) (err error) {
 	if a, e := safe.GetText(run, op.Kind); e != nil {
 		err = e
 	} else if b, e := safe.GetText(run, op.OtherKind); e != nil {
 		err = e
 	} else {
-		err = addRelation(w, rel, a.String(), b.String(), true, false)
+		err = addRelation(cat, rel, a.String(), b.String(), true, false)
 	}
 	return
 }
 
-func (op *DefineRelation) addManyToMany(w weaver.Weaves, run rt.Runtime, rel string) (err error) {
+func (op *DefineRelation) addManyToMany(cat *weave.Catalog, run rt.Runtime, rel string) (err error) {
 	if a, e := safe.GetText(run, op.Kind); e != nil {
 		err = e
 	} else if b, e := safe.GetText(run, op.OtherKind); e != nil {
 		err = e
 	} else {
-		err = addRelation(w, rel, a.String(), b.String(), true, true)
+		err = addRelation(cat, rel, a.String(), b.String(), true, true)
 	}
 	return
 }
 
-func addRelation(w weaver.Weaves, rel, a, b string, amany, bmany bool) error {
+func addRelation(cat *weave.Catalog, rel, a, b string, amany, bmany bool) error {
 	rn, an, bn := inflect.Normalize(rel), inflect.Normalize(a), inflect.Normalize(b)
-	return w.AddRelation(rn, an, bn, amany, bmany)
+	// this can spin a bit until all members of the relation are known
+	// having it in ancestry ensures that the kinds will exist for the property phase
+	return cat.Schedule(weaver.AncestryPhase, func(w weaver.Weaves, run rt.Runtime) error {
+		return w.AddRelation(rn, an, bn, amany, bmany)
+	})
 }
