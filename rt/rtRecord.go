cd79939a31c6d5727a3d336e7190bb7064574228
ionous <github@ionous.net>
2024-05-04 21:42:14 -0700

    improve comments and error messages

--- rt/rtRecord.go
+++ rt/rtRecord.go
@@ -6,8 +6,11 @@ import (
 	"git.sr.ht/~ionous/tapestry/affine"
 )
 
-// Record - provides low level access to named field/values pairs.
+// Record - provides access to named field/values pairs.
 // The fields of a record are defined by its kind.
+// Fields that are themselves records ( ie. sub-records ) start uninitialized.
+// Trying to Get() an uninitialized record will result in a "NilRecord" error.
+// safe.EnsureField() can use the Kinds database to create sub-records on demand.
 type Record struct {
 	*Kind
 	values []Value
@@ -35,6 +38,7 @@ func NewRecordByName(ks Kinds, cls string) (ret *Record, err error) {
 }
 
 // GetNamedField picks a value or trait from this record.
+// Can return a NilRecord error for uninitialized record fields.
 func (d *Record) GetNamedField(field string) (ret Value, err error) {
 	// note: the field is a trait when the field that was found doesnt match the field requested
 	if i := d.FieldIndex(field); i < 0 {
@@ -54,7 +58,7 @@ func (d *Record) GetNamedField(field string) (ret Value, err error) {
 
 // GetIndexedField panics if out of range.
 // note: traits are never indexed fields ( although their aspect is )
-// fix? GetIndexedField writes defaults into the record if there was no value.
+// Can return a NilRecord error for uninitialized record fields.
 func (d *Record) GetIndexedField(i int) (ret Value, err error) {
 	// is the stored value valid? return it
 	if fv, ft := d.values[i], d.Field(i); fv != nil {
@@ -86,7 +90,7 @@ func (d *Record) GetIndexedField(i int) (ret Value, err error) {
 }
 
 // SetNamedField - pokes the passed value into the record.
-// Unlike the Value interface, this doesnt panic and it doesnt copy values.
+// Unlike the Value interface, this doesn't panic and it doesn't copy values.
 func (d *Record) SetNamedField(field string, val Value) (err error) {
 	if i := d.FieldIndex(field); i < 0 {
 		err = UnknownField(d.Name(), field)
@@ -97,7 +101,7 @@ func (d *Record) SetNamedField(field string, val Value) (err error) {
 		} else {
 			// set the aspect to the value of the requested trait
 			if yes := val.Affinity() == affine.Bool && val.Bool(); !yes {
-				err = fmt.Errorf("error setting trait: couldn't determine the meaning of %q %s", field, val.Affinity())
+				err = fmt.Errorf("couldn't set trait %q because couldn't understand %s", field, val.Affinity())
 			} else {
 				d.values[i] = StringFrom(field, ft.Type)
 			}
@@ -107,11 +111,10 @@ func (d *Record) SetNamedField(field string, val Value) (err error) {
 }
 
 // SetIndexedField - note this doesn't handle trait translation.
-// Unlike the Value interface, this doesnt panic and it doesnt copy values.
+// Unlike the Value interface, this doesn't panic and it doesn't copy values.
+// As a special case, passing a nil value will reset the field to its default.
 func (d *Record) SetIndexedField(i int, val Value) (err error) {
 	if val == nil {
-		// probably a terrible idea: resets status of the value to default
-		// ( ex. for event interrupt )
 		d.values[i] = val
 	} else {
 		var okay bool
@@ -120,14 +123,12 @@ func (d *Record) SetIndexedField(i int, val Value) (err error) {
 		// the most flexible: anything can fit into anything.
 		case affine.Bool, affine.Number, affine.Text, affine.TextList, affine.NumList:
 			okay = aff == ft.Affinity
-
 		// the least flexible: exact matches are needed.
 		case affine.Record, affine.RecordList:
 			okay = aff == ft.Affinity && cls == ft.Type
-
 		}
 		if !okay {
-			err = fmt.Errorf("couldnt set field %s ( %s of type %q ) with val %s of type %q", ft.Name, ft.Affinity, ft.Type, aff, cls)
+			err = fmt.Errorf("couldnt set field %q ( %s of %q ) with val %s of type %q", ft.Name, ft.Affinity, ft.Type, aff, cls)
 		} else {
 			d.values[i] = val
 		}
