f2ca529c314b00648d90549e90e42ccf3795d5e2
ionous <github@ionous.net>
2024-06-08 10:07:53 -0700

    remove unneeded "grammar_maker"

--- dl/grammar/grammarStarts.go
+++ dl/grammar/grammarStarts.go
@@ -4,11 +4,6 @@ import (
 	"git.sr.ht/~ionous/tapestry/parser"
 )
 
-// GrammarMaker -
-type GrammarMaker interface{ MakeGrammar() GrammarMaker }
-
-func (op *Directive) MakeGrammar() GrammarMaker { return op }
-
 // acts as AllOf{Words, ... }
 func (op *Directive) MakeScanners() (ret parser.Scanner) {
 	return makeSequence(op.Series)
