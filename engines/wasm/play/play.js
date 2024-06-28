e79f74180e8caae934cdf3d012f07c6a3f697115
ionous <github@ionous.net>
2024-06-28 09:27:06 -0700

    Update play.js

--- engines/wasm/play/play.js
+++ engines/wasm/play/play.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import Play from './Play.vue'         // contains the router-view
 
 const go = new Go();
-WebAssembly.instantiateStreaming(fetch("/tap.wasm.gz"), go.importObject).then((result) => {
+WebAssembly.instantiateStreaming(fetch("/tap.wasm"), go.importObject).then((result) => {
     go.run(result.instance);
     let app = createApp(Play);
     app.mount('#play');
